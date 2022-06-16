7a463e199f931d1842eb539d4e4d9404f2ca0617
Karimof <islamkarimov8580@gmail.com>
2022-06-16 09:43:15 +0500

    Init the empty app

---
+++ src/main/java/uz/quickly_write_html/QuicklyWriteHtmlApplication.java
@@ -0,0 +1,13 @@
+package uz.quickly_write_html;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicklyWriteHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuicklyWriteHtmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/quickly_write_html/QuicklyWriteHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.quickly_write_html;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicklyWriteHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
